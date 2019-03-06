b73eeeddb31acb12a3dc87661b84a78aea7beffe
maggiekim <jinkyeong91@gmail.com>
2019-03-05 23:17:12 -0800

    [JavaScript] Object & Array & File Division
    - var Object & array with push()
    - make *.js and <script src="*.js"> enhance the effectiveness

---
+++ CSS/color.js
@@ -0,0 +1,37 @@
+// OBJECT
+var Body = {
+  setColor:function(color) {
+    document.querySelector('body').style.color = color;
+      },  // important to write comma(,) between properties
+      setBackgroundColor:function(color) {
+        document.querySelector('body').style.backgroundColor = color;
+      }
+    }
+    var Links = {
+      setColor:function(color) {
+        var selectorList = document.querySelectorAll('a');
+        for(var i = 0; i < selectorList.length; i++)
+        {
+        // console.log(selectorList[i]);
+        selectorList[i].style.color = color;
+      }
+    }
+  }
+
+// FUNCTION
+function changeBasicColor(curVal) {
+  if(curVal.value === 'night')
+  {
+    Body.setBackgroundColor('#99ffcc');
+    Body.setColor('white');
+    Links.setColor('green');
+    curVal.value = "day";
+  }
+  else
+  {
+    Body.setBackgroundColor('#ff4d94');
+    Body.setColor('white');
+    Links.setColor('powderblue');
+    curVal.value = "night";
+  }
+}
