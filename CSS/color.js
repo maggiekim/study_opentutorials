ad0ae5da614e4da098070cb81d0c500638567711
maggiekim <jinkyeong91@gmail.com>
2019-03-11 22:22:45 -0700

    [JavaScript] libary / framework / UI / API
    - using jQuery
    - make your own project / if you block read "limit_guide_road"

--- CSS/color.js
+++ CSS/color.js
@@ -1,20 +1,23 @@
 // OBJECT
 var Body = {
   setColor:function(color) {
-    document.querySelector('body').style.color = color;
+    // document.querySelector('body').style.color = color;
+    $('body').css('color', color);
       },  // important to write comma(,) between properties
       setBackgroundColor:function(color) {
-        document.querySelector('body').style.backgroundColor = color;
+        // document.querySelector('body').style.backgroundColor = color;
+        $('body').css('backgroundColor', color);
       }
     }
     var Links = {
       setColor:function(color) {
-        var selectorList = document.querySelectorAll('a');
-        for(var i = 0; i < selectorList.length; i++)
-        {
-        // console.log(selectorList[i]);
-        selectorList[i].style.color = color;
-      }
+      //   var selectorList = document.querySelectorAll('a');
+      //   for(var i = 0; i < selectorList.length; i++)
+      //   {
+      //   // console.log(selectorList[i]);
+      //   selectorList[i].style.color = color;
+      // }
+      $('a').css('color', color);
     }
   }
 
